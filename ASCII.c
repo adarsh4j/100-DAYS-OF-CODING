4b254395e1d709b99a6b2eea033abaf03f0fb302
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-11-18 19:16:34 +0530

    Create ASCII.c

---
+++ ASCII.c
@@ -0,0 +1,12 @@
+      //Write a program to find ascii value of a character in c language//
+
+                            //solution//
+
+#include <stdio.h>
+int main() {  
+	char c;
+	printf("Enter a character: ");
+	scanf("%c", &c);  
+	printf("ASCII value of %c = %d", c, c);
+	return 0;
+}
