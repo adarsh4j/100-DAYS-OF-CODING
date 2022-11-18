2a6bf2f468acea3f3f355a2f498125c28bb9932e
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-11-18 19:19:02 +0530

    Create ASCII.c

---
+++ DAY3/ASCII.c
@@ -0,0 +1,12 @@
+//PROGRAM TO FIND THE ASCII VALUE OF A CHARACTER//
+
+           //SOLUTION//
+
+#include <stdio.h>
+int main() {  
+	char c;
+	printf("Enter a character: ");
+	scanf("%c", &c);  
+	printf("ASCII value of %c = %d", c, c);
+	return 0;
+}
