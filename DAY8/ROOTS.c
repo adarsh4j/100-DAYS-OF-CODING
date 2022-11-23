e13580ad69ed881cf5330a34df234e5f1d118790
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-11-23 18:37:01 +0530

    Create ROOTS.c

---
+++ DAY8/ROOTS.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+	float a,b,c,d,root1,root2,x,y;
+	printf("Enter the value of a,b,c : ");
+	scanf("%f %f %f",&a,&b,&c);
+	d = (b*b-4*a*c);
+	if(d>0)
+	{
+        printf("Roots are distinct\n");
+		root1 = (-b+sqrt(d))/(2*a);
+		root2 = (-b-sqrt(d))/(2*a);
+		printf("Roots are %.2f , %.2f",root1,root2);
+	}
+	if(d == 0)
+	{
+		printf("Roots are equal\n");
+		root1 = (-b)/(2*a);
+		printf("Root1=Root2=%.2f",root1);
+	}
+	if(d<0)
+	{
+		x = (-b)/(2*a);
+		y = sqrt(-(d))/(2*a);
+		printf("Roots are\n");
+		printf("%.2f + i%.2f , ",x,y);
+		printf("\n%.2f - i%.2f",x,y);
+	}
+	return 0;
+}
