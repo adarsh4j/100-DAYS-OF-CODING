5635c8ee745837a419e430aca336c96ba8cf814d
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2022-12-06 06:58:46 +0530

    Create Prime.c

---
+++ Day20/Prime.c
@@ -0,0 +1,36 @@
+#include<stdio.h>  
+
+int main(){    
+
+int n,i,flag=0;    
+
+printf("Enter the number to check prime:");    
+
+scanf("%d",&n);        
+
+for(i=2;i<=(n/2);i++)    
+
+{    
+
+if(n%i==0)    
+
+{    
+
+printf("%d is not prime number",n);    
+
+flag=1;    
+
+break;    
+
+}    
+
+}    
+
+if(flag==0)    
+
+printf("%d is prime number",n);     
+
+return 0;
+
+}
+
