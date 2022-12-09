0926fa7ba6c020292d0b8e906b52052af214b0c1
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2022-12-09 23:08:31 +0530

    Create Pyramid1.c

---
+++ Day24/Pyramid1.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	int a,i,j,k;	scanf("%d",&a);
+
+	for(i=1;i<=a;i++)
+
+	{
+
+		for(j=a;j>i;j--)
+
+		{
+
+			printf(" ");
+
+		}
+
+		for(k=1;k<=2*i-1;k++)
+
+		{
+
+			printf("*");
+
+		}
+
+		printf("\n");
+
+		printf("\n");
+
+		printf("\n");
+
+	//	printf("\n");
+
+	}
+
+	return  0;
+
+}
+
+//Output
+
+    
+
+    *
+
+   ***
+
+   
+
+  *****
+
+  
+
+ *******     
