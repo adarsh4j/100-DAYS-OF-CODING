c6c140d6040d9336cbd6300525a19d5969903bbd
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2022-12-10 17:49:25 +0530

    Find area of circle

---
+++ Day25/Area.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	cout<<"Enter the radius:"<<endl;
+	cin>>r;
+	cout<<"The area of the circle is "<<endl;
+	cout<<(3.14*r*r)<<endl;
+	return 0;
+}
