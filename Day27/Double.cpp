dd398bfb6f668d30048607a3f8de72ed6d4fc1e6
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2022-12-12 21:48:31 +0530

    Find the double without airthmetic operator

---
+++ Day27/Double.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int a,b,i;
+
+cout<<"Enter the number:"<<endl;
+
+cin>>a;
+
+b=a;
+
+for(i=0;i<b;i++)
+
+{
+
+    a++;
+
+}
+
+cout<<"The double of "<<b<<" is "<<a<<endl;return 0;
+
+}
+
