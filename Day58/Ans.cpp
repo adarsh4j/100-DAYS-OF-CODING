66faf5374ffc0e665d4298aa6db41d94d1b15b80
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2023-03-10 07:26:18 +0530

    Create Ans.cpp

---
+++ Day58/Ans.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int t,a,b,i;
+
+    cin>>t;
+
+    while (t--)
+
+    {
+
+        int a,b;
+
+        cin>>a;
+
+        cin>>b;      
+
+        cout<<(a-b);
+
+    }
+
+    
+
+    return 0;
+
+}
+
