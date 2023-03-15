498c7e844d46249e6c983ed4444935319f94683e
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2023-03-15 08:38:57 +0530

    Create Ans.cpp

---
+++ Day62/Ans.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+ int t;
+
+cin>>t;
+
+ while(t--){
+
+ int n,x,y;
+
+cin>>n>>x>>y;
+
+if((n+1)*y>=x)
+
+ {
+
+ 	cout<<"YES"<<endl; }
+
+ else
+
+ {
+
+ 	cout<<"NO"<<endl;
+
+ }
+
+ }
+
+ return 0;
+
+ }
